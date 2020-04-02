ca3401459f1e4176914fb90a7614bbaf12b2677e
Willames Alves <wyllamesro@gmail.com>
2020-04-01 21:07:39 -0300

    add utilitário de formataão de data

--- utils.js
+++ utils.js
@@ -17,12 +17,23 @@ module.exports = {
     const today = new Date()
     const birtDate = new Date(timestamp)
 
-    let age = today.getFullYear() - birtDate.getUTCFullYear()
+    let age = today.getFullYear() - birtDate.getFullYear()
     const month = today.getMonth() - birtDate.getMonth()
 
     if( month < 0 || month == 0 && today.getDate() <= birtDate.getDate())
     age = age -1
 
     return age
+  }, 
+
+  date: timestamp =>{
+
+    const date = new Date(timestamp)
+
+    const year = date.getUTCFullYear()
+    const month = `0${date.getUTCMonth()+1}`.slice(-2)
+    const day = `0${date.getUTCDate()}`.slice(-2)
+
+    return `${year}-${month}-${day}`
   }
 }
