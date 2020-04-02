1ee65e1a0ffb2e07f9e972c3dc876ca107039957
Willames Alves <wyllamesro@gmail.com>
2020-04-01 21:05:50 -0300

    adição rota de editar e deletar

--- routes.js
+++ routes.js
@@ -7,9 +7,7 @@ routes.get('/', (req, res) => {
   return res.redirect('/instructors')
 })
 
-routes.get('/instructors', (req, res) => {
-  return res.render('instructors/index')
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/instructors/create', (req, res) =>{
   return res.render('instructors/create')
@@ -17,11 +15,13 @@ routes.get('/instructors/create', (req, res) =>{
 
 routes.get('/instructors/:id', instructors.show)
 
+routes.get('/instructors/:id/edit', instructors.edit)
+
 routes.post('/instructors', instructors.create)
 
-routes.get('/instructors/:id/edit', (req, res)=>{
-  return res.render('instructors/edit')
-})
+routes.put('/instructors', instructors.put)
+
+routes.delete('/instructors', instructors.delete)
 
 routes.get('/members', (req, res) => {
   return res.send('members')
